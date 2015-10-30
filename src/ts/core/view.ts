7da76b637bfdc09b3fb640289274221028e5990a
yajamon <yajamon.tatsuki@gmail.com>
2015-10-30 15:54:34 +0900

    implements Subject to View

--- src/ts/core/view.ts
+++ src/ts/core/view.ts
@@ -2,11 +2,23 @@ namespace YJMCNT.Core {
     /**
      * View
      */
-    export class View implements Observer {
+    export class View implements Observer, Subject {
+        observerList:Observer[]; 
+
         constructor() {
         }
         
         update():void {
+        }
+        
+        addObserver(observer:Observer):void {
+            this.observerList.push(observer);
+        }
+        notifyObservers():void {
+            for (var index = 0; index < this.observerList.length; index++) {
+                var observer = this.observerList[index];
+                observer.update();
+            }
         }
         
         render():Element {
