2af71abad650d45866fe95320e31ca4d393a2e55
yajamon <yajamon.tatsuki@gmail.com>
2015-11-20 22:49:31 +0900

    Add logic count reset

--- src/ts/model/counter.ts
+++ src/ts/model/counter.ts
@@ -4,10 +4,12 @@ namespace YJMCNT {
      */
     export class Counter extends Core.Model {
         private _value:number;
+        private _defaltValue:number;
         
         constructor() {
             super();
-            this._value = 0;
+            this.value = 0;
+            this.defaltValue = 0;
         }
         
         up (val:number){
@@ -17,6 +19,11 @@ namespace YJMCNT {
         
         down (val:number){
             this.value -= val;
+            this.notifyObservers();
+        }
+        
+        reset (){
+            this.value = this.defaltValue;
             this.notifyObservers();
         }
         
@@ -32,5 +39,13 @@ namespace YJMCNT {
             return this._value;
         }
 
+        private set defaltValue(v : number) {
+            this._defaltValue = Math.ceil(v);
+        }
+        
+        private get defaltValue() : number {
+            return this._defaltValue;
+        }
+        
     }
 }
