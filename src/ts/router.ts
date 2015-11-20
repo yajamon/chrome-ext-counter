b4e72f0b18841a1d3a5eef1f21c1c7dced117c10
yajamon <yajamon.tatsuki@gmail.com>
2015-11-20 21:33:50 +0900

    Use jQuery ready

--- src/ts/router.ts
+++ src/ts/router.ts
@@ -1,6 +1,6 @@
 /// <reference path="../../typings/jquery/jquery" />
 
-window.addEventListener("load", function() {
+$(function(){
     var dom: Element = document.querySelector('.yjmcnt-index');
     if (dom != null) {
         var counter = new YJMCNT.CounterController(dom);
