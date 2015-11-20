d175d29ad6cdde36b9c133031e34364f38a9a8b4
yajamon <yajamon.tatsuki@gmail.com>
2015-11-20 21:32:30 +0900

    Add ref comment

--- src/ts/router.ts
+++ src/ts/router.ts
@@ -1,3 +1,5 @@
+/// <reference path="../../typings/jquery/jquery" />
+
 window.addEventListener("load", function() {
     var dom: Element = document.querySelector('.yjmcnt-index');
     if (dom != null) {
