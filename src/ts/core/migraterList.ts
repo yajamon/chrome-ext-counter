8db89ec1adb7c82d73b055222854b678c407dc75
yajamon <yajamon.tatsuki@gmail.com>
2015-12-14 21:51:41 +0900

    Add class: MigraterList

---
+++ src/ts/core/migraterList.ts
@@ -0,0 +1,25 @@
+/// <reference path="migrater" />
+
+namespace YJMCNT.Core {
+    /**
+     * MigraterList
+     */
+    export class MigraterList {
+        constructor(private migraters:Migrater[]) {
+        }
+        /**
+         * migration
+         */
+        public migration(event:IDBVersionChangeEvent) {
+            var target = <IDBOpenDBRequest>event.target;
+            var db = <IDBDatabase>target.result;
+            var oldVersion = event.oldVersion || 0;
+            var newVersion = event.newVersion;
+            
+            for (var index = oldVersion; index <= newVersion; index++) {
+                var element = this.migraters[index];
+                element.up(db);
+            }
+        }
+    }
+}
