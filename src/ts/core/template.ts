6124a5e00d73298aa0ecf954143754ef635eb939
yajamon <yajamon.tatsuki@gmail.com>
2015-12-18 21:17:01 +0900

    Add template base class;

---
+++ src/ts/core/template.ts
@@ -0,0 +1,15 @@
+/// <reference path="../../../typings/jquery/jquery" />
+
+namespace YJMCNT.Core {
+    /**
+     * Template
+     */
+    export class Template {
+        constructor() {
+        }
+        
+        render(): JQuery {
+            return $();
+        }
+    }
+}
