1e8a36e388afae24d2df8850b4f1d819cda1a9b7
yajamon <yajamon.tatsuki@gmail.com>
2015-12-20 16:43:03 +0900

    Add couner factoryMethod

--- src/ts/model/counter.ts
+++ src/ts/model/counter.ts
@@ -17,6 +17,11 @@ namespace YJMCNT {
             this.defaltValue = 0;
         }
 
+        static make():Counter {
+            var c = new Counter();
+            return c;
+        }
+
         private generateUUID():string {
             return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
                 var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
