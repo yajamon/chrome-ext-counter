1cacbce521f0789432f4f8f1b5c53026e4133bb8
yajamon <yajamon.tatsuki@gmail.com>
2015-12-20 20:26:47 +0900

    counterListTemplateを実装

--- src/ts/template/counterList.ts
+++ src/ts/template/counterList.ts
@@ -16,7 +16,12 @@ namespace YJMCNT {
         render() {
             var context = $("<div>");
 
-
+            for (var index = 0; index < this.counters.length; index++) {
+                var counter = this.counters[index];
+                var template = new CounterTemplate();
+                template.count = counter.show();
+                context.append(template.render());
+            }
 
             return context;
         }
