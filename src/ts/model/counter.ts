aafe0b2120cf5230e527093ad64684eb80c8bee9
yajamon <yajamon.tatsuki@gmail.com>
2015-12-21 02:03:07 +0900

    fix format
    fixed #3, fixed #6

--- src/ts/model/counter.ts
+++ src/ts/model/counter.ts
@@ -6,8 +6,8 @@ namespace YJMCNT {
      * Counter
      */
     export class Counter extends Core.Model {
-        private _id:string;
-        private _value:number;
+        private _id: string;
+        private _value: number;
         private _defaltValue: number;
 
         static observerList: Core.Observer[] = [];
@@ -22,14 +22,14 @@ namespace YJMCNT {
             this.defaltValue = 0;
         }
 
-        static make():Counter {
+        static make(): Counter {
             var c = new Counter();
             return c;
         }
 
-        private generateUUID():string {
+        private generateUUID(): string {
             return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-                var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+                var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
                 return v.toString(16);
             });
         }
@@ -43,7 +43,7 @@ namespace YJMCNT {
             return data;
         }
 
-        static deserialize(data:CountersSchema):Counter {
+        static deserialize(data: CountersSchema): Counter {
             var counter = this.make();
             counter._id = data.id;
             counter.value = data.value;
@@ -56,17 +56,17 @@ namespace YJMCNT {
             this.saveToStore();
         }
 
-        down (val:number){
+        down(val: number) {
             this.value -= val;
             this.saveToStore();
         }
 
-        reset (){
+        reset() {
             this.value = this.defaltValue;
             this.saveToStore();
         }
 
-        saveToStore () {
+        saveToStore() {
             var transaction = this.db.transaction([this.storeName], Config.DB.READWRITE);
             var store = transaction.objectStore(this.storeName);
             var request = store.put(this.serialize());
@@ -75,27 +75,27 @@ namespace YJMCNT {
             }
         }
 
-        show (){
+        show() {
             return this._value;
         }
 
-        public get id() : string {
+        public get id(): string {
             return this._id;
         }
 
-        private set value(v : number) {
+        private set value(v: number) {
             this._value = Math.ceil(v);
         }
 
-        private get value() : number {
+        private get value(): number {
             return this._value;
         }
 
-        private set defaltValue(v : number) {
+        private set defaltValue(v: number) {
             this._defaltValue = Math.ceil(v);
         }
 
-        private get defaltValue() : number {
+        private get defaltValue(): number {
             return this._defaltValue;
         }
 
